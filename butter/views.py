6fc25891b3b548115345dca949b26242d82cf1d7
Zainab Siddiqui <zainabsiddiqui8@gmail.com>
2020-04-07 00:26:13 -0400

    added functionality to add project with multiple selected categories

--- butter/views.py
+++ butter/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Project
+from .models import Project, Category
+from django.views.generic import CreateView
+from django.http import HttpResponseRedirect
+from django.utils.text import slugify
+
 
 def project_list(request):
 	return render(request, 'butter/project_list.html')
@@ -9,3 +13,23 @@ def project_detail(request, project_slug):
 	expense_list = project.expenses
 	return render(request, 'butter/project_detail.html', {'project': project, 'expense_list': project.expenses.all()})
 
+class ProjectCreateView(CreateView):
+    model = Project
+    template_name = 'butter/add_project.html'
+    fields = ('name', 'budget')
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+
+        categories = self.request.POST['categoriesString'].split(',')
+        for category in categories:
+            Category.objects.create(
+                project=Project.objects.get(id=self.object.id),
+                name=category
+            ).save()
+
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return slugify(self.request.POST['name'])
