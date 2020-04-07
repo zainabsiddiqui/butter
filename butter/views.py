acb81a34779bb88507077b27b90531a88cec1d0d
Zainab Siddiqui <zainabsiddiqui8@gmail.com>
2020-04-07 13:35:15 -0400

    added functionality for users to be able to add expenses

--- butter/views.py
+++ butter/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Project, Category
+from .models import Project, Category, Expense
 from django.views.generic import CreateView
 from django.http import HttpResponseRedirect
 from django.utils.text import slugify
+from .forms import ExpenseForm
 
 
 def project_list(request):
@@ -10,26 +11,46 @@ def project_list(request):
 
 def project_detail(request, project_slug):
 	project = get_object_or_404(Project, slug = project_slug)
-	expense_list = project.expenses
-	return render(request, 'butter/project_detail.html', {'project': project, 'expense_list': project.expenses.all()})
+	
+	if request.method == 'GET':
+		category_list = Category.objects.filter(project =project)
+		return render(request, 'butter/project_detail.html', {'project': project, 'expense_list': project.expenses.all(), 'category_list': category_list})
+	elif request.method == 'POST':
+		# process the form
+		form = ExpenseForm(request.POST)
+
+		if form.is_valid():
+			title = form.cleaned_data['title']
+			amount = form.cleaned_data['amount']
+			category_name = form.cleaned_data['category']
+
+			category = get_object_or_404(Category, project = project, name = category_name)
+
+			Expense.objects.create(
+				project = project, 
+				title = title,
+				amount = amount,
+				category = category).save()
+
+	return HttpResponseRedirect(project_slug)
 
 class ProjectCreateView(CreateView):
-    model = Project
-    template_name = 'butter/add_project.html'
-    fields = ('name', 'budget')
+	model = Project
+	template_name = 'butter/add_project.html'
+	fields = ('name', 'budget')
 
-    def form_valid(self, form):
-        self.object = form.save(commit=False)
-        self.object.save()
+	def form_valid(self, form):
+		self.object = form.save(commit=False)
+		self.object.save()
 
-        categories = self.request.POST['categoriesString'].split(',')
-        for category in categories:
-            Category.objects.create(
-                project=Project.objects.get(id=self.object.id),
-                name=category
-            ).save()
+		categories = self.request.POST['categoriesString'].split(',')
+		for category in categories:
+			Category.objects.create(
+				project=Project.objects.get(id=self.object.id),
+				name=category
+			).save()
 
-        return HttpResponseRedirect(self.get_success_url())
+		return HttpResponseRedirect(self.get_success_url())
 
-    def get_success_url(self):
-        return slugify(self.request.POST['name'])
+	def get_success_url(self):
+		return slugify(self.request.POST['name'])
