b87d5379ae676f6384a55cf174e5c266755ed947
Zainab Siddiqui <zainabsiddiqui8@gmail.com>
2020-04-08 00:05:56 -0400

    added user login, logout functionality and a nav bar to all pages

--- butter/urls.py
+++ butter/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 from users import views as user_views
@@ -6,6 +7,9 @@ from users import views as user_views
 urlpatterns = [
     path('', views.project_list, name='list'),
     path('register', user_views.register, name = 'register'),
+    path('login', auth_views.LoginView.as_view(template_name = 'users/login.html'), name = 'login'),
+    path('logout', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name = 'logout'),
+    path('profile', user_views.profile, name = 'profile'),
     path('add', views.ProjectCreateView.as_view(), name = 'add'),
     path('<slug:project_slug>', views.project_detail, name='detail')
 ]
