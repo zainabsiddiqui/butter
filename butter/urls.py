860276a2861681f095d9e289d79c19386973efbb
Zainab Siddiqui <zainabsiddiqui8@gmail.com>
2020-04-15 21:10:56 -0400

    added profile feature and ability to update it

--- butter/urls.py
+++ butter/urls.py
@@ -3,6 +3,8 @@ from django.contrib.auth import views as auth_views
 from django.urls import path
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 
 urlpatterns = [
@@ -12,10 +14,18 @@ urlpatterns = [
     path('about', views.about, name = 'about'),
     path('register', user_views.register, name = 'register'),
     path('login', auth_views.LoginView.as_view(template_name = 'users/login.html'), name = 'login'),
+    path('password-reset', auth_views.PasswordResetView.as_view(template_name = 'users/password_reset.html'), 
+        name = 'password_reset'),
+    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name = 'users/password_reset_done.html'), 
+        name = 'password_reset_done'),
+    path('password-reset/confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name = 'users/password_reset_confirm.html'), 
+        name = 'password_reset_confirm'),
     path('logout', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name = 'logout'),
     path('profile', user_views.profile, name = 'profile'),
     path('add', views.ProjectCreateView.as_view(), name = 'add'),
     path('<slug:project_slug>', views.project_detail, name='detail'),
     url(r'^delete/(?P<project_pk>.*)$', views.delete, name='delete-project'),
-    # path('software-engineering-class-project', views.project_detail, name='detail')
-]
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
